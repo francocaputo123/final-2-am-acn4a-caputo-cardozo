892d189b43c9b6f3566499b12c1b35eae422e3f5
Gian Franco Miguel Caputo <gianfrancocap12@gmail.com>
2023-10-10 18:43:58 -0300

    created methods and modified register view

--- app/src/main/java/com/example/learnyourself/MainActivity.java
+++ app/src/main/java/com/example/learnyourself/MainActivity.java
@@ -9,6 +9,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MainActivity extends AppCompatActivity {
     private EditText userText, userPassword;
 
@@ -16,15 +20,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //userText = findViewById(R.id.userText);
-        //userPassword = findViewById(R.id.userPassword);
+        userText = findViewById(R.id.userEdit);
+        userPassword = findViewById(R.id.passEdit);
     }
 
     //Método para el botón de registro: cambia de activity a la pagina registro.
-    public void register(View v)
+    public void registerAc(View v)
     {
         Intent i = new Intent(this, Register.class);
         startActivity(i);
     }
 
+    public void login(){
+
+
+    }
 }
--- app/src/main/java/com/example/learnyourself/Register.java
+++ app/src/main/java/com/example/learnyourself/Register.java
@@ -1,20 +1,52 @@
 package com.example.learnyourself;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class Register extends AppCompatActivity {
 
+    private EditText nt, lnt, unt, up;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        nt = findViewById(R.id.nameText);
+        lnt = findViewById(R.id.lastNameText);
+        unt = findViewById(R.id.userNameText);
+        up = findViewById(R.id.passwordText);
     }
 
-    /*public boolean addUser(){
+    public void regis(){
+        String userRegister = unt.getText().toString();
+        String nameText = nt.getText().toString();
+        String lastNameText = lnt.getText().toString();
+        String userNameText = unt.getText().toString();
+        String passwordText = up.getText().toString();
 
-        return false
-    }*/
+        try
+        {
+            OutputStreamWriter user = new OutputStreamWriter(openFileOutput(userRegister, Context.MODE_PRIVATE));
+            user.write(nameText);
+            user.write(lastNameText);
+            user.write(userNameText);
+            user.write(passwordText);
+            user.flush();
+            nt.setText("");
+            lnt.setText("");
+            unt.setText("");
+            up.setText("");
+            Toast.makeText(this,"Usuario registrado con éxito, vuelva a la pantalla inicial e ingrese con su usuario", Toast.LENGTH_SHORT);
+        } catch(IOException e){
+            Toast.makeText(this, "No se pudo crear el usuario, intentelo denuevo", Toast.LENGTH_SHORT);
+        }
+    }
     public void preView()
     {
         finish();
