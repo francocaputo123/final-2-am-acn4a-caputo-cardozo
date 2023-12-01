86d8123952008d7b8e6efc920dc56e9da200ce0d
CardozoFede <fdcardozo178@gmail.com>
2023-12-01 16:42:57 -0300

    Create Phyton

--- app/src/main/java/com/example/learnyourself/Phyton.java
+++ app/src/main/java/com/example/learnyourself/Phyton.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Java extends AppCompatActivity {
+public class Phyton extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_java);
+        setContentView(R.layout.activity_phyton);
     }
 }
