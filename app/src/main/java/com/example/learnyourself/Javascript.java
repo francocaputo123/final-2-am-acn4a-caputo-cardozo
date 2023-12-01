3040cb4d5e4574998e839fd912e5c5c1a330d4c4
CardozoFede <fdcardozo178@gmail.com>
2023-12-01 16:33:06 -0300

    Create Java

--- app/src/main/java/com/example/learnyourself/Javascript.java
+++ app/src/main/java/com/example/learnyourself/Javascript.java
@@ -2,15 +2,20 @@ package com.example.learnyourself;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class Javascript extends AppCompatActivity {
 
-    @Override
+       @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_javascript);
+
     }
 
     public void volver(View v)
