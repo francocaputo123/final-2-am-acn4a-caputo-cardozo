e2ab77530a8dee24e2365ef3136a8e09bcc57d85
CardozoFede <fdcardozo178@gmail.com>
2023-12-15 14:52:57 -0300

    avanzando

--- app/src/main/java/com/example/learnyourself/ImagenDowloader.java
+++ app/src/main/java/com/example/learnyourself/ImagenDowloader.java
@@ -1,16 +1,27 @@
 package com.example.learnyourself;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.util.Log;
+
+import java.io.InputStream;
+import java.net.URL;
 
 public class ImagenDowloader extends AsyncTask<String, Integer, Bitmap> {
     private Bitmap imagenDescargada = null;
     @Override
     protected Bitmap doInBackground(String... strings)
     {
-        String URL = strings[0];
-        Log.i("testing", URL);
+        String URL_consulta = strings[0];
+        try {
+            URL consulta = new URL(URL_consulta);
+            InputStream contenido = (InputStream)  consulta.getContent();
+            BitmapFactory.decodeStream(contenido);
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
         return imagenDescargada;
     }
 }
